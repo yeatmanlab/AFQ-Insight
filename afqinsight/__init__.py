87b88959570cabd77ddd60d440ac7cfd32441ac4
Adam Richie-Halford <richford@uw.edu>
2018-02-25 09:46:11 -0800

    Add datasets.py for customized make_classification and make_sparse_group_classification

--- afqinsight/__init__.py
+++ afqinsight/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import, division, print_function
 
-from .transform import *  # noqa
 from .forests import *  # noqa
+from .transform import *  # noqa
 from .version import __version__  # noqa
