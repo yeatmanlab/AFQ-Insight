8d56453be80e379e7b389de4056de316eb170ce0
Adam Richie-Halford <richford@uw.edu>
2018-02-26 11:15:19 -0800

    Add datasets to __init__.py

--- afqinsight/__init__.py
+++ afqinsight/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, division, print_function
 
+import .datasets
 from .forests import *  # noqa
 from .transform import *  # noqa
 from .version import __version__  # noqa
