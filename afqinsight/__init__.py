c5cd5da56d818f3a749ed7f35660e93a88ce13a5
Adam Richie-Halford <richford@uw.edu>
2018-03-23 17:19:52 -0700

    Add insight.py

--- afqinsight/__init__.py
+++ afqinsight/__init__.py
@@ -2,5 +2,6 @@ from __future__ import absolute_import, division, print_function
 
 from . import datasets
 from .forests import *  # noqa
+from .insight import *  # noqa
 from .transform import *  # noqa
 from .version import __version__  # noqa
