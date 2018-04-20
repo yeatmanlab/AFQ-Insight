c53c6a7652f3f1c1e76b1b4825859c701a5cf59b
Adam Richie-Halford <richford@uw.edu>
2018-04-20 14:00:16 -0700

    Remove forests.py from __init__ import. Import only load_afq_data from datasets in __init__

--- afqinsight/__init__.py
+++ afqinsight/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, division, print_function
 
 from . import datasets  # noqa
-from .forests import *  # noqa
+from .datasets import load_afq_data  # noqa
 from .insight import *  # noqa
 from .transform import *  # noqa
 from .version import __version__  # noqa
