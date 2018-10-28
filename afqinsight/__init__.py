30612362b6feece5129b7c0a80b5ebdc6889347a
Adam Richie-Halford <richford@uw.edu>
2018-10-27 20:49:01 -0700

    Add plot submodule to __init__.py

--- afqinsight/__init__.py
+++ afqinsight/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, division, print_function
 
 from . import datasets  # noqa
+from . import plot  # noqa
 from . import utils  # noqa
 from .datasets import *  # noqa
 from .insight import *  # noqa
