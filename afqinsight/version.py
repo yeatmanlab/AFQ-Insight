7fb35e2ce7f31a6abe3837e98840a1c9cff1f2fa
Adam Richie-Halford <richford@uw.edu>
2019-05-10 17:18:40 -0700

    Add hyperopt to requirements

--- afqinsight/version.py
+++ afqinsight/version.py
@@ -66,6 +66,7 @@ PACKAGE_DATA = {'afqinsight': [pjoin('data', '*', '*'), ]}
 REQUIRES = [
     "copt @ git+git://github.com/openopt/copt.git",
     "numpy>=1.11.3", "pandas>=0.22.0", "scipy>=1.0.0",
+    "hyperopt>=0.1.2",
     "scikit-learn>=0.19.1", "tqdm>=4.19.4",
 ]
 EXTRAS_REQUIRE = {}
