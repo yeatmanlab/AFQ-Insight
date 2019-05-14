6049080caa4dd5a93e11971c66ee156a1f3c3a2c
Adam Richie-Halford <richford@uw.edu>
2019-05-14 10:37:12 -0700

    Add black as a pre-commit hook

--- afqinsight/version.py
+++ afqinsight/version.py
@@ -5,8 +5,8 @@ from os.path import join as pjoin
 # Format expected by setup.py and doc/source/conf.py: string of form "X.Y.Z"
 _version_major = 0
 _version_minor = 1
-_version_micro = ''  # use '' for first of series, number for 1 and above
-_version_extra = 'dev'
+_version_micro = ""  # use '' for first of series, number for 1 and above
+_version_extra = "dev"
 # _version_extra = ''  # Uncomment this for full releases
 
 # Construct full version string from these.
@@ -16,19 +16,23 @@ if _version_micro:
 if _version_extra:
     _ver.append(_version_extra)
 
-__version__ = '.'.join(map(str, _ver))
+__version__ = ".".join(map(str, _ver))
 
-CLASSIFIERS = ["Development Status :: 3 - Alpha",
-               "Environment :: Console",
-               "Intended Audience :: Science/Research",
-               "License :: OSI Approved :: MIT License",
-               "Operating System :: OS Independent",
-               "Programming Language :: Python",
-               "Topic :: Scientific/Engineering"]
+CLASSIFIERS = [
+    "Development Status :: 3 - Alpha",
+    "Environment :: Console",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Topic :: Scientific/Engineering",
+]
 
 # Description should be a one-liner:
-description = ("afq-insight: a python library to analyze diffusion "
-               "tensor imaging results in the AFQ data format.")
+description = (
+    "afq-insight: a python library to analyze diffusion "
+    "tensor imaging results in the AFQ data format."
+)
 # Long description will go up on the pypi page
 long_description = """
 AFQ-Insight
@@ -62,20 +66,16 @@ MAJOR = _version_major
 MINOR = _version_minor
 MICRO = _version_micro
 VERSION = __version__
-PACKAGE_DATA = {'afqinsight': [pjoin('data', '*', '*'), ]}
+PACKAGE_DATA = {"afqinsight": [pjoin("data", "*", "*")]}
 REQUIRES = [
     "copt @ git+git://github.com/openopt/copt.git",
-    "numpy>=1.11.3", "pandas>=0.22.0", "scipy>=1.0.0",
+    "numpy>=1.11.3",
+    "pandas>=0.22.0",
+    "scipy>=1.0.0",
     "hyperopt>=0.1.2",
     "scikit-learn>=0.19.1",
     "tqdm>=4.19.4",
     "ipywidgets",
 ]
-EXTRAS_REQUIRE = {
-    "dev": [
-        "flake8",
-        "pytest",
-        "pytest-cov",
-    ]
-}
+EXTRAS_REQUIRE = {"dev": ["flake8", "pytest", "pytest-cov", "pre-commit"]}
 ENTRY_POINTS = {}
