f93535792323baa22ff3d58c6630ffc724b92ec0
Adam Richie-Halford <richford@uw.edu>
2019-05-14 17:18:09 -0700

    Formatting for test files. And use_pickle in test_transform.py

--- afqinsight/tests/test_transform.py
+++ afqinsight/tests/test_transform.py
@@ -10,13 +10,15 @@ test_data_path = op.join(data_path, "test_data")
 
 
 def test_AFQFeatureTransformer():
-    nodes_path = op.join(data_path, "nodes.csv")
+    nodes_path = op.join(test_data_path, "nodes.csv")
     nodes = pd.read_csv(nodes_path)
     transformer = afqi.AFQFeatureTransformer()
-    x, groups, cols = transformer.transform(nodes)
+    x, groups, cols, bias_idx = transformer.transform(nodes)
 
-    x_ref = np.load(op.join(test_data_path, "test_transform_x.npy"))
-    groups_ref = np.load(op.join(test_data_path, "test_transform_groups.npy"))
+    x_ref = np.load(op.join(test_data_path, "test_transform_x.npy"), allow_pickle=True)
+    groups_ref = np.load(
+        op.join(test_data_path, "test_transform_groups.npy"), allow_pickle=True
+    )
     cols_ref = pd.read_pickle(op.join(test_data_path, "test_transform_cols.pkl"))
 
     assert np.all(x == x_ref)
@@ -31,72 +33,82 @@ def test_isiterable():
 
 
 def test_remove_group():
-    x_ref = np.load(op.join(test_data_path, "test_transform_x.npy"))
+    x_ref = np.load(op.join(test_data_path, "test_transform_x.npy"), allow_pickle=True)
     label_sets_ref = np.load(
-        op.join(test_data_path, "test_multicol2sets_label_sets.npy")
+        op.join(test_data_path, "test_multicol2sets_label_sets.npy"), allow_pickle=True
     )
 
     x_removed = afqi.remove_group(x_ref, ("Callosum Forceps Major",), label_sets_ref)
 
-    x_removed_ref = np.load(op.join(test_data_path, "test_remove_group_x.npy"))
+    x_removed_ref = np.load(
+        op.join(test_data_path, "test_remove_group_x.npy"), allow_pickle=True
+    )
 
     assert np.all(x_removed == x_removed_ref)
 
 
 def test_remove_groups():
-    x_ref = np.load(op.join(test_data_path, "test_transform_x.npy"))
+    x_ref = np.load(op.join(test_data_path, "test_transform_x.npy"), allow_pickle=True)
     label_sets_ref = np.load(
-        op.join(test_data_path, "test_multicol2sets_label_sets.npy")
+        op.join(test_data_path, "test_multicol2sets_label_sets.npy"), allow_pickle=True
     )
 
     x_removed = afqi.remove_groups(
         x_ref, [("Callosum Forceps Major",), ("Uncinate",), ("fa",)], label_sets_ref
     )
 
-    x_removed_ref = np.load(op.join(test_data_path, "test_remove_groups_x.npy"))
+    x_removed_ref = np.load(
+        op.join(test_data_path, "test_remove_groups_x.npy"), allow_pickle=True
+    )
 
     assert np.all(x_removed == x_removed_ref)
 
 
 def test_select_group():
-    x_ref = np.load(op.join(test_data_path, "test_transform_x.npy"))
+    x_ref = np.load(op.join(test_data_path, "test_transform_x.npy"), allow_pickle=True)
     label_sets_ref = np.load(
-        op.join(test_data_path, "test_multicol2sets_label_sets.npy")
+        op.join(test_data_path, "test_multicol2sets_label_sets.npy"), allow_pickle=True
     )
 
     x_select = afqi.select_group(x_ref, ("Callosum Forceps Major",), label_sets_ref)
 
-    x_select_ref = np.load(op.join(test_data_path, "test_select_group_x.npy"))
+    x_select_ref = np.load(
+        op.join(test_data_path, "test_select_group_x.npy"), allow_pickle=True
+    )
 
     assert np.all(x_select == x_select_ref)
 
 
 def test_select_groups():
-    x_ref = np.load(op.join(test_data_path, "test_transform_x.npy"))
+    x_ref = np.load(op.join(test_data_path, "test_transform_x.npy"), allow_pickle=True)
     label_sets_ref = np.load(
-        op.join(test_data_path, "test_multicol2sets_label_sets.npy")
+        op.join(test_data_path, "test_multicol2sets_label_sets.npy"), allow_pickle=True
     )
 
     x_select = afqi.select_groups(
         x_ref, [("Callosum Forceps Major",), ("Uncinate",), ("fa",)], label_sets_ref
     )
 
-    x_select_ref = np.load(op.join(test_data_path, "test_select_groups_x.npy"))
+    x_select_ref = np.load(
+        op.join(test_data_path, "test_select_groups_x.npy"), allow_pickle=True
+    )
 
     assert np.all(x_select == x_select_ref)
 
 
 def test_shuffle_group():
-    x_ref = np.load(op.join(test_data_path, "test_transform_x.npy"))
+    x_ref = np.load(op.join(test_data_path, "test_transform_x.npy"), allow_pickle=True)
     label_sets_ref = np.load(
-        op.join(test_data_path, "test_multicol2sets_label_sets.npy")
+        op.join(test_data_path, "test_multicol2sets_label_sets.npy"), allow_pickle=True
     )
 
     x_shuffle = afqi.shuffle_group(
         x_ref, ("Corticospinal",), label_sets_ref, random_seed=42
     )
 
-    x_shuffle_ref = np.load(op.join(test_data_path, "test_shuffle_group_x.npy"))
+    x_shuffle_ref = np.load(
+        op.join(test_data_path, "test_shuffle_group_x.npy"), allow_pickle=True
+    )
 
     assert np.all(x_shuffle == x_shuffle_ref)
 
@@ -106,7 +118,7 @@ def test_multicol2sets():
 
     label_sets = afqi.multicol2sets(cols)
     label_sets_ref = np.load(
-        op.join(test_data_path, "test_multicol2sets_label_sets.npy")
+        op.join(test_data_path, "test_multicol2sets_label_sets.npy"), allow_pickle=True
     )
 
     assert np.all(label_sets == label_sets_ref)
