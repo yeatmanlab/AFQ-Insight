96701a677f9f3b16d8e2ae21c2a6b267cf4b3a79
Adam Richie-Halford <richiehalford@gmail.com>
2020-09-05 21:09:27 -0700

    Pin copt and update for copt API changes. Rename regularization params to alpha and lamda_

--- afqinsight/prox.py
+++ afqinsight/prox.py
@@ -20,18 +20,18 @@ class SparseGroupL1(object):
     Implements the sparse group lasso penalty [1]_
 
     .. math::
-        (1 - \alpha_1) * \alpha_2 \displaystyle \sum_{g \in G} || \beta_g ||_2
-        + \alpha_1 * \alpha_2 || \beta ||_1
+        (1 - \alpha) * \lambda \displaystyle \sum_{g \in G} || \beta_g ||_2
+        + \alpha * \lambda || \beta ||_1
 
     where :math:`G` is a partition of the features into groups.
 
     Parameters
     ----------
-    alpha_1 : float
-        Combination between group lasso and lasso. alpha_1 = 0 gives the group
-        lasso and alpha_1 = 1 gives the lasso.
+    alpha : float
+        Combination between group lasso and lasso. alpha = 0 gives the group
+        lasso and alpha = 1 gives the lasso.
 
-    alpha_2 : float
+    lambda_ : float
         Regularization parameter, overall strength of regularization.
 
     groups : numpy.ndarray
@@ -55,16 +55,19 @@ class SparseGroupL1(object):
         DOI: 10.1080/10618600.2012.681250
     """  # noqa: W605
 
-    def __init__(self, alpha_1, alpha_2, groups, bias_index=None):
-        self.alpha_1 = alpha_1
-        self.alpha_2 = alpha_2
+    def __init__(self, alpha, lambda_, groups, bias_index=None):
+        self.alpha = alpha
+        self.lambda_ = lambda_
         self.groups = groups
+        self.sqrt_group_lengths = np.sqrt(
+            np.sum(np.ones_like(np.array(groups)), axis=1)
+        )
         self.bias_index = bias_index
 
     def __call__(self, x):
         penalty = (
-            (1.0 - self.alpha_1)
-            * self.alpha_2
+            (1.0 - self.alpha)
+            * self.lambda_
             * np.sum([np.linalg.norm(x[g]) for g in self.groups])
         )
 
@@ -72,7 +75,7 @@ class SparseGroupL1(object):
         if self.bias_index is not None:
             ind[self.bias_index] = False
 
-        penalty += self.alpha_1 * self.alpha_2 * np.abs(x[ind]).sum()
+        penalty += self.alpha * self.lambda_ * np.abs(x[ind]).sum()
         return penalty
 
     def prox(self, x, step_size):
@@ -83,16 +86,18 @@ class SparseGroupL1(object):
         .. math::
             P(\beta) = P_1(\beta) + P_2(\beta)
 
-        where :math:`P_2 = \alpha_1 \alpha_2 || \beta ||_1` is the lasso
-        penalty and :math:`P_1 = (1 - \alpha_1) \alpha_2 \displaystyle
+        where :math:`P_2 = \alpha \lambda || \beta ||_1` is the lasso
+        penalty and :math:`P_1 = (1 - \alpha) \lambda \displaystyle
         \sum_{g \in G} || \beta_g ||_2` is the group lasso penalty.
 
         Then the proximal operator is given by
 
         .. math::
-            \text{prox}_{\lambda P_1 + \lambda P_2} (u) = \left(
-            \text{prox}_{\lambda P_2} \circ \text{prox}_{\lambda P_1}
+            \text{prox}_{\sigma P_1 + \sigma P_2} (u) = \left(
+            \text{prox}_{\sigma P_2} \circ \text{prox}_{\sigma P_1}
             \right)(u)
+        
+        where :math:`sigma` is a step size
 
         Parameters
         ----------
@@ -108,17 +113,20 @@ class SparseGroupL1(object):
             proximal operator of sparse group lasso penalty evaluated on
             input `x` with step size `step_size`
         """  # noqa: W605
-        l1_prox = np.fmax(x - self.alpha_1 * self.alpha_2 * step_size, 0) - np.fmax(
-            -x - self.alpha_1 * self.alpha_2 * step_size, 0
+        l1_prox = np.fmax(x - self.alpha * self.lambda_ * step_size, 0) - np.fmax(
+            -x - self.alpha * self.lambda_ * step_size, 0
         )
         out = l1_prox.copy()
 
         if self.bias_index is not None:
             out[self.bias_index] = x[self.bias_index]
 
-        norms = np.linalg.norm(l1_prox[self.groups], axis=1)
+        norms = (
+            np.linalg.norm(l1_prox[np.array(self.groups)], axis=1)
+            / self.sqrt_group_lengths
+        )
 
-        norm_mask = norms > (1.0 - self.alpha_1) * self.alpha_2 * step_size
+        norm_mask = norms > (1.0 - self.alpha) * self.lambda_ * step_size
         mask_idx_true = np.where(norm_mask)[0]
         mask_idx_false = np.where(np.logical_not(norm_mask))[0]
         groups_true = np.array(self.groups)[mask_idx_true]
@@ -126,8 +134,8 @@ class SparseGroupL1(object):
 
         out[groups_true] -= (
             step_size
-            * (1.0 - self.alpha_1)
-            * self.alpha_2
+            * (1.0 - self.alpha)
+            * self.lambda_
             * out[groups_true]
             / norms[mask_idx_true, np.newaxis]
         )
