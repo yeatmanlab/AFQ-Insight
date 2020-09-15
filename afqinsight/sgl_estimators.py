8ad19aa53265cce83c0d6526b4aa2b5ca4816951
Adam Richie-Halford <richiehalford@gmail.com>
2020-09-15 06:03:40 -0700

    Change default alpha and lambd

--- afqinsight/sgl_estimators.py
+++ afqinsight/sgl_estimators.py
@@ -29,11 +29,11 @@ class SGLEstimator(BaseEstimator):
 
     Parameters
     ----------
-    alpha : float, default=0.0
+    alpha : float, default=1.0
         Hyper-parameter : Combination between group lasso and lasso. alpha=0
         gives the group lasso and alpha=1 gives the lasso.
 
-    lambd : float, default=0.0
+    lambd : float, default=1.0
         Hyper-parameter : overall regularization strength.
 
     fit_intercept : bool, default=True
@@ -93,8 +93,8 @@ class SGLEstimator(BaseEstimator):
 
     def __init__(
         self,
-        alpha=0.0,
-        lambd=0.0,
+        alpha=1.0,
+        lambd=1.0,
         fit_intercept=True,
         max_iter=5000,
         tol=1e-6,
@@ -259,11 +259,11 @@ class SGLRegressor(SGLEstimator, RegressorMixin, LinearModel):
 
     Parameters
     ----------
-    alpha : float, default=0.0
+    alpha : float, default=1.0
         Hyper-parameter : Combination between group lasso and lasso. alpha=0
         gives the group lasso and alpha=1 gives the lasso.
 
-    lambd : float, default=0.0
+    lambd : float, default=1.0
         Hyper-parameter : overall regularization strength.
 
     fit_intercept : bool, default=True
@@ -372,11 +372,11 @@ class SGLClassifier(SGLEstimator, LinearClassifierMixin):
 
     Parameters
     ----------
-    alpha : float, default=0.0
+    alpha : float, default=1.0
         Hyper-parameter : Combination between group lasso and lasso. alpha=0
         gives the group lasso and alpha=1 gives the lasso.
 
-    lambd : float, default=0.0
+    lambd : float, default=1.0
         Hyper-parameter : overall regularization strength.
 
     fit_intercept : bool, default=True
