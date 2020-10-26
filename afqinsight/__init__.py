be8edf6244517922ca66e0da223f1563e23659cb
Adam Richie-Halford <richiehalford@gmail.com>
2020-10-26 15:23:28 -0700

    ENH: Add cross_validate_checkpoint

--- afqinsight/__init__.py
+++ afqinsight/__init__.py
@@ -1,6 +1,7 @@
 """AFQ-Insight is a Python library for statistical learning of tractometry data."""
 from . import datasets  # noqa
 from . import utils  # noqa
+from .cross_validate import *  # noqa
 from .datasets import *  # noqa
 from .pipeline import *  # noqa
 from .transform import *  # noqa
