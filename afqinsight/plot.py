323e68af8a605495d0baed022b579eb9792b041e
Adam Richie-Halford <richiehalford@gmail.com>
2021-03-18 14:56:59 -0700

    Remove pyAFQ dependency

--- afqinsight/plot.py
+++ afqinsight/plot.py
@@ -1,14 +1,55 @@
 """Plot bundle profiles."""
 
-import AFQ.data as afqd
-import AFQ.viz.utils as plot_utils
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
 
+from collections import OrderedDict
 from groupyr.transform import GroupExtractor
 from tqdm.auto import tqdm
+
+from .utils import BUNDLE_MAT_2_PYTHON
+
+
+POSITIONS = OrderedDict(
+    {
+        "ATR_L": (1, 0),
+        "ATR_R": (1, 4),
+        "C_L": (1, 0),
+        "C_R": (1, 4),
+        "CST_L": (1, 1),
+        "CST_R": (1, 3),
+        "CGC_L": (3, 1),
+        "CGC_R": (3, 3),
+        "MCP": (3, 1),
+        "CCMid": (3, 3),
+        "FP": (4, 2),
+        "FA": (0, 2),
+        "CC_ForcepsMinor": (4, 2),
+        "CC_ForcepsMajor": (0, 2),
+        "IFO_L": (4, 1),
+        "IFO_R": (4, 3),
+        "IFOF_L": (4, 1),
+        "IFOF_R": (4, 3),
+        "HCC_L": (4, 0),
+        "HCC_R": (4, 4),
+        "ILF_L": (3, 0),
+        "ILF_R": (3, 4),
+        "F_L": (3, 0),
+        "F_R": (3, 4),
+        "SLF_L": (2, 1),
+        "SLF_R": (2, 3),
+        "ARC_L": (2, 0),
+        "ARC_R": (2, 4),
+        "AF_L": (2, 0),
+        "AF_R": (2, 4),
+        "UNC_L": (0, 1),
+        "UNC_R": (0, 3),
+        "UF_L": (0, 1),
+        "UF_R": (0, 3),
+    }
+)
 
 
 def plot_tract_profiles(
@@ -136,7 +177,7 @@ def plot_tract_profiles(
 
         # Arrange the bundles into a grid
         bgcolor = "white"
-        positions = plot_utils.POSITIONS.copy()
+        positions = POSITIONS.copy()
         positions["FA"] = (0, 0)
         positions["FP"] = (0, 3)
         for bid in positions.keys():
@@ -148,7 +189,7 @@ def plot_tract_profiles(
         fig, axes = plt.subplots(nrows=5, ncols=4, sharex=True)
 
         for tid, df_stat in tqdm(tract_stats.items()):
-            bundle_id = afqd.BUNDLE_MAT_2_PYTHON.get(tid, tid)
+            bundle_id = BUNDLE_MAT_2_PYTHON.get(tid, tid)
 
             if "HCC_" in bundle_id or "Cingulum Hippocampus" in tid:
                 continue
