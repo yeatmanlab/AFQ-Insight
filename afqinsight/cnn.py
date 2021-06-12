e252c47acc9d99c4cefa4e12230d0a44880bcf7e
ceciliabarnes <cbarnes7@uw.edu>
2021-06-12 07:11:38 -0700

    need to add docstring and additional arguments

--- afqinsight/cnn.py
+++ afqinsight/cnn.py
@@ -23,14 +23,13 @@ def build_model(hp, conv_layers, input_shape):
 	model.add(Dense(dense_filters, activation='relu', input_shape=input_shape))
 
 	for i in range(conv_layers):
-		model.add(Conv1D(filters=hp.Int('filters' + str(i), min_value=32, max_value=512, step=32),
-		                 kernel_size=hp.Int('kernel' + str(i), min_value=1, max_value=4, step=1),
+		model.add(Conv1D(filters=hp.Int('conv_filters' + str(i), min_value=32, max_value=512, step=32),
+		                 kernel_size=hp.Int('conv_kernel' + str(i), min_value=1, max_value=4, step=1),
 		                 activation='relu'))
 
 		model.add(MaxPool1D(pool_size=2, padding='same'))
 
-		model.add(Dropout(0.25))
-
+	model.add(Dropout(0.25))
 	model.add(Flatten())
 
 	dense_filters_2 = hp.Int('dense_filters_2', min_value=32, max_value=512, step=32)
@@ -48,12 +47,15 @@ class ModelBuilder:
 	This class controls the building of complex model architecture and the number of layers in the model.
 	"""
 
-	def __init__(self, class_type, input_shape, layers, max_epochs, val_split, **tuner_kwargs):
+	def __init__(self, class_type, input_shape, layers, max_epochs, X_test, y_test, batch_size, val_split, **tuner_kwargs):
 		self.class_type = class_type
 		self.layers = layers
 		self.input_shape = input_shape
 		self.max_epochs = max_epochs
+		self.batch_size = batch_size
 		self.val_split = val_split
+		self.X_test = X_test
+		self.y_test = y_test
 		self.tuner_kwargs = tuner_kwargs
 
 	def _get_tuner(self):
@@ -64,21 +66,21 @@ class ModelBuilder:
 			if self.class_type == "hyperband":
 				tuner = kt.Hyperband(hypermodel=hypermodel,
 				                     objective='mean_squared_error',
-				                     max_epochs=self.max_epochs,
+				                     max_epochs=10,
 				                     overwrite=True,
 				                     **self.tuner_kwargs)
 
 			elif self.class_type == "bayesian":
 				tuner = kt.BayesianOptimization(hypermodel=hypermodel,
 				                                objective='mean_squared_error',
-				                                max_trials=self.max_epochs,
+				                                max_trials=10,
 				                                overwrite=True,
 				                                **self.tuner_kwargs)
 
 			elif self.class_type == "random":
 				tuner = kt.RandomSearch(hypermodel=hypermodel,
 				                        objective='mean_squared_error',
-				                        max_trials=self.max_epochs,
+				                        max_trials=10,
 				                        overwrite=True,
 				                        **self.tuner_kwargs)
 			else:
@@ -99,15 +101,15 @@ class ModelBuilder:
 			verbose=True)
 
 		# Fitting model using model checkpoint callback to find best model which is saved to 'weights'
-		model.fit(X, y, epochs=self.max_epochs, callbacks=[model_checkpoint_callback], validation_split=self.val_split)
-
+		#model.fit(X, y, epochs=self.max_epochs, batch_size=self.batch_size, callbacks=[model_checkpoint_callback], validation_split=self.val_split)
+		model.fit(X, y, epochs=self.max_epochs, batch_size=self.batch_size, callbacks=[model_checkpoint_callback],
+		          validation_data=(self.X_test, self.y_test))
 		# loading in weights
 		model.load_weights(weights_path)
 
 		# return the model
 		return model
 
-	# TODO: Ask if I should have tuning on this
 	def build_basic_model(self, X, y):
 		model = Sequential()
 		model.add(Dense(128, activation='relu', input_shape=X.shape[1:]))
@@ -138,7 +140,7 @@ class ModelBuilder:
 		tuner = self._get_tuner()
 
 		# Find the optimal hyperparameters
-		tuner.search(X, y, epochs=50, validation_split=self.val_split)
+		tuner.search(X, y, epochs=50, validation_split=0.2)
 
 		# Save the optimal hyperparameters
 		best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
@@ -155,7 +157,7 @@ class CNN:
 	This class implements the common sklearn model interface (has a fit and predict function).
 	"""
 
-	def __init__(self, nodes, channels, max_epochs, tuner=None, layers=1, val_split=0.2, **tuner_kwargs):
+	def __init__(self, nodes, channels, max_epochs=50, batch_size=32, tuner=None, layers=1, val_split=0.2, **tuner_kwargs):
 		"""
 		Constructs a CNN that uses the given number of nodes, each with a
 		max depth of max_depth.
@@ -183,6 +185,11 @@ class CNN:
 			raise TypeError("Parameter max_epochs must be an integer.")
 		else:
 			self.max_epochs = max_epochs
+
+		if not type(batch_size) is int:
+			raise TypeError("Parameter max_epochs must be an integer.")
+		else:
+			self.batch_size = batch_size
 
 		# checking tiner is passed as str or None
 		if type(tuner) is not str and tuner is not None:
@@ -216,28 +223,29 @@ class CNN:
 
 		subjects = X.shape[0]
 
-		# nodes * channels must = X.shape[1]
-		if self.nodes * self.channels != X.shape[1]:
-			raise ValueError("The product nodes and channels is not the correct shape.")
-		# error
-		else:
-			X = np.swapaxes(X.reshape((subjects, self.channels, self.nodes)), 1, 2)
+
+		X = np.swapaxes(X.reshape((subjects, self.channels, self.nodes)), 1, 2)
 
 		if y is not None:
 			return X, y
 		else:
 			return X
 
-	def fit(self, X, y):
+	def fit(self, X, y, X_test, y_test):
 		"""
 		Takes an input dataset X and a series of targets y and trains the CNN.
 		"""
-
-		X, y = self._preprocess(X, y)
-
+		# nodes * channels must = X.shape[1]
+		if self.nodes * self.channels != X.shape[1]:
+			raise ValueError("The product nodes and channels is not the correct shape.")
+		# error
+		else:
+			X, y = self._preprocess(X, y)
+		X_test, y_test = self._preprocess(X_test, y_test)
 		# CNN gets nodes, channels, max_epochs, tuner=None, layers=None
 		# Model Builder takes class_type, input_shape, layers, max_epochs, **kwargs
-		builder = ModelBuilder(self.tuner, X.shape[1:], self.layers, self.max_epochs, self.val_split, **self.tuner_kwargs)
+		builder = ModelBuilder(self.tuner, X.shape[1:], self.layers, self.max_epochs, X_test, y_test,
+		                       self.batch_size, self.val_split, **self.tuner_kwargs)
 		if self.tuner is None:
 			self.model_ = builder.build_basic_model(X, y)
 		else:
@@ -253,7 +261,7 @@ class CNN:
 		"""
 		X = self._preprocess(X)
 		check_is_fitted(self, 'is_fitted_')
-		pred = self.model_.predict(X)
+		pred = self.model_.predict(X).squeeze()
 		return pred
 
 	def score(self, y_test, y_hat):
