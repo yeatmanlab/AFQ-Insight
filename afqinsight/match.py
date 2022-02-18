6504b6891ba2f7ae013d50aceb2ab1ec72f811e9
36000 <jk6.28@outlook.com>
2022-02-17 17:27:59 -0800

    simpler API

--- afqinsight/match.py
+++ afqinsight/match.py
@@ -1,85 +1,85 @@
 """Perform statistical matching comparing test and control units."""
 
+from os import name
 import numpy as np
+import pandas as pd
 from scipy.optimize import linear_sum_assignment
 
 
-__all__ = [
-    "mahalonobis_dist_match_df", "mahalonobis_dist_match",
-    "mahalonobis_dist_arr"]
+__all__ = ["mahalonobis_dist_match"]
 
 
-def mahalonobis_dist_match_df(data, id_column, test_ids, ctrl_ids,
-                              feature_columns, threshold=0.2):
+def mahalonobis_dist_match(
+    data=None, test=None, ctrl=None, status_col=None, feature_cols=None, threshold=0.2
+):
     """
-    Perform Mahalanobis distance matching (MDM) on a pandas
-    dataframe.
+    If data is None, then test and ctrl must be arrays.
+    If data is provided, then status_col and feature_cols are used.
 
     Parameters
     ----------
-    data : pandas Dataframe
+    data : pandas Dataframe, optional
         The feature samples for test subjects.
-    id_column : str
-        Column with unique subject identifiers.
-    test_ids : list of str
-        List of subject identifiers for test subjects.
-    ctrl_ids : list of str
-        List of subject identifiers for control subjects.
-    feature_columns : list of str
-        List of columns to use as features for MDM.
-    threshold : float
-        Mahalnobis distance beyond which matches are not accepted.
-
-    Returns
-    -------
-    filtered_test_ids : array-like
-        List of subject identifiers for successfully matched test subjects
-    filtered_ctrl_ids : array-like
-        List of subject identifiers for matched control subjects,
-        this array will be the same length as filtered_test_ids
-    """
-    test_X = data[data[id_column].isin(test_ids)][feature_columns].to_numpy()
-    ctrl_X = data[data[id_column].isin(ctrl_ids)][feature_columns].to_numpy()
-    filtered_test_ind, filtered_ctrl_ind = mahalonobis_dist_match(
-        test_X, ctrl_X, threshold=threshold)
-    test_ids = np.asarray(test_ids)
-    ctrl_ids = np.asarray(ctrl_ids)
-    return test_ids[filtered_test_ind], ctrl_ids[filtered_ctrl_ind]
-
-
-def mahalonobis_dist_match(test_X, ctrl_X, threshold=0.2):
-    """
-    Perform Mahalanobis distance matching (MDM) between two arrays.
-
-    Parameters
-    ----------
-    test_X : array-like of shape (n_samples1, n_features)
+        Default: None
+    test : array-like of shape (n_samples1, n_features), optional
         The feature samples for test subjects.
-    ctrl_X : array-like of shape (n_samples2, n_features)
+        Default: None
+    ctrl : array-like of shape (n_samples2, n_features), optional
         Feature samples for control subjects from which
         matches will be selected.
-    threshold : float
+        Default: None
+    status_col : str, optional
+        Column with subject status. Must have only two unique values,
+        evaluating to True and False, where True indicates a test subject
+        and False indicates a control.
+        Default: None
+    feature_cols : list of str, optional
+        List of columns to use as features for MDM.
+        If feature_columns is None then we use all of the columns
+        except status_col as the features.
+        Default: None
+    threshold : float, optional
         Mahalnobis distance beyond which matches are not accepted.
+        Default: None
 
     Returns
     -------
-    filtered_test_ind : array-like
-        Indices into test_X of successfully matched test subjects
-    filtered_ctrl_ind : array-like
-        Indices into ctrl_X of matched control subjects,
-        this array will be the same length as filtered_test_ind
+    
     """
-    test_X = np.asarray(test_X)
-    ctrl_X = np.asarray(ctrl_X)
+    if data is not None:
+        if status_col is None:
+            raise ValueError(
+                ("If data is provided," " status_col must also be provided")
+            )
 
-    if np.any(np.isnan(test_X)) or np.any(np.isnan(ctrl_X)):
-        raise ValueError((
-            "There are NaNs in test_X or ctrl_X. "
-            "Please replace these NaNs using interpolation or by removing "
-            "the subjects with NaNs before calling mahalonobis_dist_match. "))
+        if len(data[status_col].unique()) != 2:
+            raise ValueError(
+                ("status_col in data must " "contain only 2 unique values")
+            )
+
+        if feature_cols is None:
+            feature_cols = data.columns
+            feature_cols.remove(status_col)
+
+        df_test_rows = data[status_col].astype(bool)
+        df_ctrl_rows = ~data[status_col].astype(bool)
+        test = data.loc[df_test_rows, feature_cols].to_numpy()
+        ctrl = data.loc[df_ctrl_rows, feature_cols].to_numpy()
+    else:
+        test = np.asarray(test)
+        ctrl = np.asarray(ctrl)
+
+    if np.any(np.isnan(test)) or np.any(np.isnan(ctrl)):
+        raise ValueError(
+            (
+                "There are NaNs in test or ctrl data. "
+                "Please replace these NaNs using interpolation or by removing "
+                "the subjects with NaNs before calling mahalonobis_dist_match. "
+            )
+        )
 
     # calculate Mahalonobis distance between test and control
-    nbrs = mahalonobis_dist_arr(test_X, ctrl_X)
+    nbrs = _mahalonobis_dist(test, ctrl)
 
     # assign neighbors using Munkres algorithm
     row_ind, col_ind = linear_sum_assignment(nbrs)
@@ -92,10 +92,30 @@ def mahalonobis_dist_match(test_X, ctrl_X, threshold=0.2):
             filtered_test_ind.append(row_idx)
             filtered_ctrl_ind.append(col_idx)
 
-    return filtered_test_ind, filtered_ctrl_ind
+    # contstruct dataframe if no dataframe is provided
+    if data is None:
+        data = {}
+        filtered_test = test[filtered_test_ind]
+        filtered_ctrl = ctrl[filtered_ctrl_ind]
+        for i in range(test.shape[1]):
+            data[f"feature_{i}"] = np.concatenate(
+                (filtered_test[:, i], filtered_ctrl[:, i]), axis=0
+            )
+        data["status"] = np.concatenate(
+            (np.ones(len(filtered_test_ind)), np.zeros(len(filtered_ctrl_ind))), axis=0
+        )
+        return pd.DataFrame(data)
+    else:
+        # determine selected rows
+        df_test_idx = np.argwhere(df_test_rows.to_numpy()).flatten()
+        df_ctrl_idx = np.argwhere(df_ctrl_rows.to_numpy()).flatten()
+        all_idx = np.concatenate(
+            (df_test_idx[filtered_test_ind], df_ctrl_idx[filtered_ctrl_ind]), axis=0
+        )
+        return data.loc[all_idx]
 
 
-def mahalonobis_dist_arr(arr1, arr2):
+def _mahalonobis_dist(arr1, arr2):
     """
     Calculate the Mahalonobis distance between two
     2-dimensional arrays along the first axis.
@@ -113,8 +133,7 @@ def mahalonobis_dist_arr(arr1, arr2):
         Mahalonobis distance between each sample in the
         two input arrays.
     """
-    v_inv = np.linalg.inv(np.cov(
-        np.concatenate((arr1, arr2), axis=0).T, ddof=0))
+    v_inv = np.linalg.inv(np.cov(np.concatenate((arr1, arr2), axis=0).T, ddof=0))
     diff = arr1[:, np.newaxis] - arr2[np.newaxis, :]
     nbrs = np.sqrt(np.sum((diff @ v_inv) * diff, axis=2))
     return nbrs
--- afqinsight/tests/test_match.py
+++ afqinsight/tests/test_match.py
@@ -3,29 +3,32 @@
 import afqinsight.match as aim
 import numpy as np
 import pandas as pd
-from sklearn.utils._testing import (
-    assert_array_almost_equal,
-    assert_array_equal)
+from sklearn.utils._testing import assert_array_almost_equal, assert_array_equal
 from scipy.spatial.distance import mahalanobis
 
 
-test_features = np.asarray([
-    [1, 2, 3, 4], [4, 2, 1, 3], [1, 4, 5, 6]])
+test_features = np.asarray([[1, 2, 3, 4], [4, 2, 1, 3], [1, 4, 5, 6]])
 
 
-ctrl_features = np.asarray([
-    [3, 2, 3, 4], [4, 5, 1, 3], [1, 10, 5, 6],
-    [1, 2, 2, 4], [4, 2, 0, 3], [1, 4, 9, 6]])
+ctrl_features = np.asarray(
+    [
+        [3, 2, 3, 4],
+        [4, 5, 1, 3],
+        [1, 10, 5, 6],
+        [1, 2, 2, 4],
+        [4, 2, 0, 3],
+        [1, 4, 9, 6],
+    ]
+)
 
 
-def test_mahalonobis_dist_arr():
-    nbrs = aim.mahalonobis_dist_arr(test_features, ctrl_features)
+def test_mahalonobis_dist():
+    nbrs = aim._mahalonobis_dist(test_features, ctrl_features)
 
-    v_inv = np.linalg.inv(np.cov(
-        np.concatenate((test_features, ctrl_features), axis=0).T, ddof=0))
-    nbrs_scipy = np.zeros((
-        test_features.shape[0],
-        ctrl_features.shape[0]))
+    v_inv = np.linalg.inv(
+        np.cov(np.concatenate((test_features, ctrl_features), axis=0).T, ddof=0)
+    )
+    nbrs_scipy = np.zeros((test_features.shape[0], ctrl_features.shape[0]))
     for ii, test_sub in enumerate(test_features):
         for jj, ctrl_sub in enumerate(ctrl_features):
             nbrs_scipy[ii, jj] = mahalanobis(test_sub, ctrl_sub, v_inv)
@@ -34,26 +37,39 @@ def test_mahalonobis_dist_arr():
 
 
 def test_mahalonobis_dist_match_df():
-    data = pd.DataFrame({
-        "eid": [34, 35, 36, 37, 38, 39, 40, 41, 42],
-        "age": [12, 345, 63, 4, 5, 6, 89, 88, 10],
-    })
+    data = pd.DataFrame(
+        {
+            "eid": [34, 35, 36, 37, 38, 39, 40, 41, 42],
+            "age": [12, 345, 63, 4, 5, 6, 89, 88, 10],
+            "status": [1, 1, 1, 0, 0, 0, 0, 0, 0],
+        }
+    )
     for ii in range(4):
         data[f"feature_{ii}"] = [*test_features[:, ii], *ctrl_features[:, ii]]
-    filtered_test_id, filtered_ctrl_id = aim.mahalonobis_dist_match_df(
-        data,
-        "eid",
-        [34, 35, 36],
-        [37, 38, 39, 40, 41, 42],
-        ["feature_0", "feature_1", "feature_2", "feature_3"],
-        threshold=1)
-    assert_array_equal(filtered_test_id, [34, 35])
-    assert_array_equal(filtered_ctrl_id, [40, 41])
+    matched_df = aim.mahalonobis_dist_match(
+        data=data,
+        status_col="status",
+        feature_cols=["feature_0", "feature_1", "feature_2", "feature_3"],
+        threshold=1,
+    )
+    assert_array_equal(
+        matched_df.loc[matched_df.status.astype(bool)].eid.to_numpy(), [34, 35]
+    )
+    assert_array_equal(
+        matched_df.loc[~matched_df.status.astype(bool)].eid.to_numpy(), [40, 41]
+    )
 
 
 def test_mahalonobis_dist_match():
-    filtered_test_ind, filtered_ctrl_ind = aim.mahalonobis_dist_match(
-        test_features, ctrl_features, threshold=1)
-    assert_array_equal(filtered_test_ind, [0, 1])
-    assert_array_equal(filtered_ctrl_ind, [3, 4])
-    assert len(filtered_test_ind) == len(filtered_ctrl_ind)
+    matched_df = aim.mahalonobis_dist_match(
+        test=test_features, ctrl=ctrl_features, threshold=1
+    )
+    assert_array_equal(
+        matched_df.loc[matched_df.status.astype(bool)].feature_2.to_numpy(), [3, 1]
+    )
+    assert_array_equal(
+        matched_df.loc[~matched_df.status.astype(bool)].feature_2.to_numpy(), [2, 0]
+    )
+    assert np.sum(matched_df.status.to_numpy() == 1) == np.sum(
+        matched_df.status.to_numpy() == 0
+    )
